e8d24a57b23eae6f76e49e7a2017085308e45423
Eric Salvi <ericsalvi@me.com>
2022-08-15 14:50:04 -0400

    additonal tailwind work for desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
         primaryViolet: "hsl(257, 40%, 49%)",
         primaryMagenta: "hsl(300, 69%, 71%)",
       },
+      backgroundImage: {
+        "desktop-pattern": "url('../images/bg-desktop.svg')",
+        "mobile-pattern": "url('../images/bg-mobile.svg')",
+      },
     },
   },
   plugins: [],

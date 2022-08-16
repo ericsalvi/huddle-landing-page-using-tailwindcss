8c6742e7f7accca5864860125d6ca13e07ced33f
Eric Salvi <ericsalvi@me.com>
2022-08-16 17:05:08 -0400

    added mobile css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
   theme: {
     fontFamily: {
       poppins: ["'Poppins'", "sans-serif"],
+      openSans: ["'Open Sans'", "sans-serif"],
     },
     extend: {
       colors: {
@@ -14,6 +15,9 @@ module.exports = {
       backgroundImage: {
         "desktop-pattern": "url('../images/bg-desktop.svg')",
         "mobile-pattern": "url('../images/bg-mobile.svg')",
+      },
+      backgroundSize: {
+        "100%": "100%",
       },
     },
   },
